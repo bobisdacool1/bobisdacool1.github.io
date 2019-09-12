a64ac185fd6caf41b22be1660cfc05f6ac0ddb03
Sexless <sinitsin.aleksey2003@yadnex.ru>
2019-09-12 11:34:04 +0300

    ProfBioService

---
+++ ProfBioService/js/main.js
@@ -0,0 +1,70 @@
+$("a").click(function () {
+  return false;
+})
+//Дезинсекция
+$('.service-item.disinsection img').on('click', function(){
+  $('.popup-wrap.disinsection').fadeIn(400)
+  $('body').toggleClass('ofh')
+})
+$('.popup-wrap.disinsection i.fa-times').on('click', function(){
+  $('.popup-wrap.disinsection').fadeOut(400)
+  $('body').toggleClass('ofh')
+})
+//Дератизация
+$('.service-item.deratization img').on('click', function(){
+  $('.popup-wrap.deratization').fadeIn(400)
+  $('body').toggleClass('ofh')
+})
+$('.popup-wrap.deratization i.fa-times').on('click', function(){
+  $('.popup-wrap.deratization').fadeOut(400)
+  $('body').toggleClass('ofh')
+})
+//Дезинфекция
+$('.service-item.disinfection img').on('click', function(){
+  $('.popup-wrap.disinfection').fadeIn(400)
+  $('body').toggleClass('ofh')
+})
+$('.popup-wrap.disinfection i.fa-times').on('click', function(){
+  $('.popup-wrap.disinfection').fadeOut(400)
+  $('body').toggleClass('ofh')
+})
+//Слайдер дезинсекция
+$('.popup-content .sub-img.disinsection').on('click', function(){
+  var mainImage, subImage
+  mainImage = $('.popup-content .main-img.disinsection').css('background-image')
+  subImage = $(this).css('background-image')
+  $(this).css('background-image', mainImage)
+  $('.popup-content .main-img.disinsection').css('background-image', subImage)
+})
+//Слайдер дератизация
+$('.popup-content .sub-img.deratization').on('click', function(){
+  var mainImage, subImage
+  mainImage = $('.popup-content .main-img.deratization').css('background-image')
+  subImage = $(this).css('background-image')
+  $(this).css('background-image', mainImage)
+  $('.popup-content .main-img.deratization').css('background-image', subImage)
+})
+//Слайдер дезинфекция
+$('.popup-content .sub-img.disinfection').on('click', function(){
+  var mainImage, subImage
+  mainImage = $('.popup-content .main-img.disinfection').css('background-image')
+  subImage = $(this).css('background-image')
+  $(this).css('background-image', mainImage)
+  $('.popup-content .main-img.disinfection').css('background-image', subImage)
+})
+
+$('.checkbox-item').on('click', function(){
+  $(this).children('.checkmark').children().toggleClass('active')
+  var price = $('input[type="number"]').val() * 6.5 * $('i.active').length
+  $('output').val(price)
+})
+$('.range').on('input change', (function(){
+  $('input[type="number"]').val($(this).val())
+}))
+$('input[type="number"]').on('input change', function(){
+  $('input[type="range"]').val($(this).val())
+})
+$('input').on('input change', function priceCount(){
+  var price = $(this).val() * 6.5 * $('i.active').length
+  $('output').val(price)
+})
