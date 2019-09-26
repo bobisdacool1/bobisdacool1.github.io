6f49b05f7b38f932466fe9d580d348daf246e2f2
kalispeller <sinitsin.aleksey2003@yandex.ru>
2019-09-26 22:01:51 +0300

    changes

--- ProfBioService/js/main.js
+++ ProfBioService/js/main.js
@@ -1,140 +1,195 @@
 $('a[href=""]').on('click', function () {
   return false;
 })
-//Дезинсекция
-$('.service-item.disinsection img').on('click', function(){
-  $('.popup-wrap.disinsection').fadeIn(400)
-  $('body').toggleClass('ofh')
+//popup enable
+$('.service-item img').on('click', function(){
+  let _class = ($(this).parent().attr('class').split(' ')[1])
+  $('.popup-wrap.' + _class).fadeIn(400)
+  $('body').addClass('ofh')
 })
-$('.popup-wrap.disinsection i.fa-times').on('click', function(){
-  $('.popup-wrap.disinsection').fadeOut(400)
-  $('body').toggleClass('ofh')
+//popup disable
+$('.popup-wrap i.fa-times').on('click', function () {
+  let _class = ($(this).parent().parent().attr('class').split(' ')[2])
+  $('.popup-wrap.' + _class).fadeOut(400)
+  $('body').removeClass('ofh')
 })
-//Дератизация
-$('.service-item.deratization img').on('click', function(){
-  $('.popup-wrap.deratization').fadeIn(400)
-  $('body').toggleClass('ofh')
-})
-$('.popup-wrap.deratization i.fa-times').on('click', function(){
-  $('.popup-wrap.deratization').fadeOut(400)
-  $('body').toggleClass('ofh')
-})
-//Дезинфекция
-$('.service-item.disinfection img').on('click', function(){
-  $('.popup-wrap.disinfection').fadeIn(400)
-  $('body').toggleClass('ofh')
-})
-$('.popup-wrap.disinfection i.fa-times').on('click', function(){
-  $('.popup-wrap.disinfection').fadeOut(400)
-  $('body').toggleClass('ofh')
-})
-$('.popup-desc .button').on('click', function(){
+$('.popup-desc .button').on('click', function () {
   $(this).parent().parent().parent().parent().parent().fadeOut(400)
   $('body').toggleClass('ofh')
-  console.log($(this))
 })
-//Слайдер дезинсекция
-$('.popup-content .sub-img.disinsection').on('click', function(){
-  var mainImage, subImage
-  mainImage = $('.popup-content .main-img.disinsection').css('background-image')
-  subImage = $(this).css('background-image')
-  $(this).css('background-image', mainImage)
-  $('.popup-content .main-img.disinsection').css('background-image', subImage)
-})
-//Слайдер дератизация
-$('.popup-content .sub-img.deratization').on('click', function(){
-  var mainImage, subImage
-  mainImage = $('.popup-content .main-img.deratization').css('background-image')
-  subImage = $(this).css('background-image')
-  $(this).css('background-image', mainImage)
-  $('.popup-content .main-img.deratization').css('background-image', subImage)
-})
-//Слайдер дезинфекция
-$('.popup-content .sub-img.disinfection').on('click', function(){
-  var mainImage, subImage
-  mainImage = $('.popup-content .main-img.disinfection').css('background-image')
-  subImage = $(this).css('background-image')
-  $(this).css('background-image', mainImage)
-  $('.popup-content .main-img.disinfection').css('background-image', subImage)
-})
-//Калькулятор
-$('.checkbox-item').on('click', function(){
-  $(this).children('.checkmark').children().toggleClass('active')
-  var price = $('.calc-input').val() * 6.5 * $('i.active').length
-  $('output').val(price)
-})
-$('.range').on('input.calc-input change', (function(){
-  $('input.calc-input[type="number"]').val($(this).val())
-}))
-$('input.calc-input[type="number"]').on('input.calc-input[type="number"] change', function(){
-  $('input[type="range"]').val($(this).val())
-})
-$('input.calc-input').on('input.calc-input change', function(){
-  var price = $(this).val() * 6.5 * $('i.active').length
-  $('output').val(price)
-})
-
-function scrollToThe(id, ofefset) {
-  if (ofefset == undefined) {
-      ofefset = 0
-      $('html, body').animate({
-          scrollTop: $(id).offset().top - ofefset
-      }, 1200)
-  }
-  else {
-      $('html, body').animate({
-          scrollTop: $(id).offset().top - ofefset
-      }, 1200)
-  }
-
-  return false
-}
-
-///////////////////////////////////////////////////////
-//feedback
-///////////////////////////////////////////////////////
-$('.popup-wrap.disinsection .second-button').on('click', function(){
-  $('.feedback-i').removeClass('active')
-  $('.disinsection-i').addClass('active')
-})
-$('.popup-wrap.disinfection .second-button').on('click', function(){
-  $('.feedback-i').removeClass('active')
-  $('.disinsection-i').addClass('active')
-})
-$('.popup-wrap.deratization .second-button').on('click', function(){
-  $('.feedback-i').removeClass('active')
-  $('.deratization-i').addClass('active')
-})
-$('.popup-wrap.disinfection .second-button').on('click', function(){
-  $('.feedback-i').removeClass('active')
-  $('.disinfection-i').addClass('active')
-})
-
-// $('document').keypress(function(){
-//   if (e.keyCode === 27){
-//     $('.navbar').css('display', 'none')
-//     $('.popup-wrap').fadeOut(400)
-//     $('body.ofh').toggleClass('ofh')
-//   }
-// })
-$(document).on('keydown', function(event) {
-  if (event.key == "Escape") {
-    $('.popup-wrap').fadeOut(400)
-    $('body.ofh').toggleClass('ofh')
+$(document).on('keyup',function(evt) {
+  if (evt.keyCode == 27) {
+     $('.popup-wrap').fadeOut(400)
+     $('body').removeClass('ofh')
   }
 });
+//carousel
+$('.popup-content .sub-img').on('click', function(){
+  let _class = ($(this).attr('class').split(' ')[2])
+  let mainImage = $('.popup-content .main-img.' + _class).css('background-image')
+  let subImage = $(this).css('background-image')
+  $(this).css('background-image', mainImage)
+  $('.popup-content .main-img.' + _class).css('background-image', subImage)
+})
+//Scroll
+function scrollToThe(id, ofefset) {
+  if (ofefset == undefined) {
+    ofefset = 0
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - ofefset
+    }, 1200)
+  }
+  else {
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - ofefset
+    }, 1200)
+  }
+  return false
+}
+//form
 $('form').on('focus', 'input[type=number]', function (e) {
   $(this).on('mousewheel.disableScroll', function (e) {
     e.preventDefault()
   })
 })
-$('.hamburger-menu').on('click', function(){
+//mobile
+$('.hamburger-menu').on('click', function () {
   $(this).toggleClass('active')
   $('.navbar-nav-m').toggleClass('active')
 })
-$('.navbar-nav__item').on('click', function(){
+$('.navbar-nav__item').on('click', function () {
   $('.navbar-nav-m').toggleClass('active')
   $('.hamburger-menu').toggleClass('active')
   $('.popup-wrap').fadeOut(400)
   $('body.ofh').toggleClass('ofh')
 })
+
+
+//Калькулятор
+$('.checkbox-item').on('click', function () {
+  $(this).children('.checkmark').children().toggleClass('active')
+  var price = priceCalc($('.price i.active'), $('input[type="range"]').val())
+  $('output').val(price)
+})
+$('.range').on('input.calc-input change', (function () {
+  $('input.calc-input[type="number"]').val($(this).val())
+}))
+$('input.calc-input[type="number"]').on('input.calc-input[type="number"] change', function () {
+  $('input[type="range"]').val($(this).val())
+})
+$('input.calc-input').on('input.calc-input change', function () {
+  var price = priceCalc($('.price i.active'), $('input[type="range"]').val())
+  $('output').val(price)
+})
+
+function priceCalc(id, amount) {
+  let _id = id.parent().parent().children('.checkmark-desc')
+  let _amount = amount
+  let _price
+  console.log($(_id[1]).attr('id'))
+  if (_id.length == 3) {
+    if (amount < 101) {
+      _price = 2600 * 2 + 2500
+    }
+    if (_amount > 100 && _amount < 501) {
+      _price = 3100 * 2 + 2900
+    }
+    if (_amount > 501 && _amount < 1001) {
+      _price = _amount * 12 * 2 + _amount * 10
+    }
+    if (_amount > 1001 && _amount < 5001) {
+      _price = _amount * 10 * 2 + _amount * 8
+    }
+    if (_amount > 5001 && _amount < 10001) {
+      _price = _amount * 7 * 2 + _amount * 5
+    }
+    if (_amount > 10000) {
+      _price = amount * 4 * 2 + _amount * 3
+    }
+  }
+  if (_id.length == 2) {
+    for (var i = 0; i < 1; i++) {
+      if ($(_id[i]).attr('id') == deratization) {
+        if (amount < 101) {
+          _price = 2500 + 2600
+        }
+        if (_amount > 100 && _amount < 501) {
+          _price = 2900 + 3100
+        }
+        if (_amount > 501 && _amount < 1001) {
+          _price = _amount * 10 + _amount * 12
+        }
+        if (_amount > 1001 && _amount < 5001) {
+          _price = _amount * 8 + _amount * 10
+        }
+        if (_amount > 5001 && _amount < 10001) {
+          _price = _amount * 5 + _amount * 7
+        }
+        if (_amount > 10000) {
+          _price = _amount * 3 + _amount * 4
+        }
+      }
+      else {
+        if (amount < 101) {
+          _price = 2600 * 2
+        }
+        if (_amount > 100 && _amount < 501) {
+          _price = 3100 * 2
+        }
+        if (_amount > 501 && _amount < 1001) {
+          _price = _amount * 12 * 2
+        }
+        if (_amount > 1001 && _amount < 5001) {
+          _price = _amount * 10 * 2
+        }
+        if (_amount > 5001 && _amount < 10001) {
+          _price = _amount * 7 * 2
+        }
+        if (_amount > 10000) {
+          _price = amount * 4 * 2
+        }
+      }
+    }
+  }
+  if (_id.length == 1 && _id.attr('id') == 'deratization') {
+    if (amount < 101) {
+      _price = 2500
+    }
+    if (_amount > 100 && _amount < 501) {
+      _price = 2900
+    }
+    if (_amount > 501 && _amount < 1001) {
+      _price = _amount * 10
+    }
+    if (_amount > 1001 && _amount < 5001) {
+      _price = _amount * 8
+    }
+    if (_amount > 5001 && _amount < 10001) {
+      _price = _amount * 5
+    }
+    if (_amount > 10000) {
+      _price = _amount * 3
+    }
+  }
+  if (_id.length == 1 && (_id.attr('id') == 'disinsection' || _id.attr('id') == 'disinfection')) {
+    if (amount < 101) {
+      _price = 2600
+    }
+    if (_amount > 100 && _amount < 501) {
+      _price = 3100
+    }
+    if (_amount > 501 && _amount < 1001) {
+      _price = _amount * 12
+    }
+    if (_amount > 1001 && _amount < 5001) {
+      _price = _amount * 10
+    }
+    if (_amount > 5001 && _amount < 10001) {
+      _price = _amount * 7
+    }
+    if (_amount > 10000) {
+      _price = amount * 4
+    }
+  }
+  return _price
+}
