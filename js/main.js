0062e9f4cb5c6b5b6a7654235880bd377a792279
Kalispeller1488 <sinitsin.aleksey2003@yandex.ru>
2019-10-01 23:39:33 +0300

    popup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-$('.work-item').on('hover', function(){
+$('.work-item').on('hover', function () {
   $(this).toggleClass('active')
+})
+$('a[href=""]').click(function (e) {
+  e.preventDefault();
+})
+//popup in
+$('.work-item-hidden a.button').on('click', function (e) {
+  e.preventDefault()
+
+  var _class = ($(this).attr('class').split(' ')[1])
+  $('.popup-wrap').removeClass('hidden')
+  $('.popup.hidden.' + _class).removeClass('hidden')
+  $('body').addClass('ofh')
+})
+
+//popup out
+$('.popup i').on('click', function (e) {
+
+  var _class = ($(this).parent().attr('class').split(' ')[1])
+  $('.popup-wrap').addClass('hidden')
+  $('.popup.' + _class).addClass('hidden')
+  $('body').removeClass('ofh')
+})
+$('.button-offer').on('click', function(e){
+  e.preventDefault()
+
+  var _class = ($(this).parent().attr('class').split(' ')[1])
+  $('.popup-wrap').addClass('hidden')
+  $('.popup.' + _class).addClass('hidden')
+  $('body').removeClass('ofh')
 })
