729d4c38826c9774bbf828063999cf19073ae77b
Kalispeller1488 <sinitsin.aleksey2003@yandex.ru>
2019-10-02 12:38:20 +0300

    js

--- js/main.js
+++ js/main.js
@@ -29,4 +29,47 @@ $('.button-offer').on('click', function(e){
   $('.popup-wrap').addClass('hidden')
   $('.popup.' + _class).addClass('hidden')
   $('body').removeClass('ofh')
+  scrollToThe('#feedback')
+})
+$(document).on('keyup',function(evt) {
+    if (evt.keyCode == 27) {
+      
+      $('.popup-wrap').addClass('hidden')
+      $('.popup').addClass('hidden')
+      $('body').removeClass('ofh')
+    }
+});
+//popup slider
+$('.sub-img').on('click', function(){
+  let sub = $(this).css('background-image')
+  let main = $(this).parent().children('.main-img')
+  $(this).css('background-image', main)
+  $(this).parent().children('.main-img').css('background-image', sub)
+})
+
+var ofefset = 0
+function scrollToThe(id, ofefset) {
+    if (ofefset == undefined) {
+        ofefset = 0
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - ofefset
+        }, 1200)
+    }
+    else {
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - ofefset
+        }, 1200)
+    }
+
+    return false
+}
+//nav
+var height = $('.navbar').offset().top
+
+$(window).on('scroll', function(){
+  if ($(this).scrollTop() > height){
+    $('.navbar').addClass('sticky')
+  } else {
+    $('.navbar').removeClass('sticky')
+  }
 })
